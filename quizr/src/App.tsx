46de066e8f853a1bf9cfa14e8aee9f915668acc6
C-Mierez <carlos.mierez20@gmail.com>
2022-03-05 14:46:09 -0300

    Layout polishing and refactor

--- quizr/src/App.tsx
+++ quizr/src/App.tsx
@@ -3,7 +3,7 @@ import "./styles/Global.module.css";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import { LayoutPage } from "./routes/Layout";
 import { HomePage } from "./routes/Home";
-import { QuizPage } from "./components/Quiz";
+import { QuizCard } from "./components/Quiz";
 import { AboutPage } from "./routes/About";
 import { NotFoundPage } from "./routes/NotFound";
 import { QuizGamePage } from "./routes/QuizGame";
@@ -15,7 +15,7 @@ function App() {
                 <Route path="/" element={<LayoutPage />}>
                     <Route index element={<HomePage />} />
                     <Route path="quiz-game" element={<QuizGamePage />}>
-                        <Route path=":quizId" element={<QuizPage />} />
+                        <Route path="" element={<QuizCard />} />
                     </Route>
                     <Route path="about" element={<AboutPage />} />
                     <Route path="*" element={<NotFoundPage />} />
